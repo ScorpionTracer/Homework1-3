7ade47313fd9c8e72b7a7498a01c2799543b188e
ScorpionTracer <badik-9494@mail.ru>
2022-12-19 12:33:54 +0500

    hw fixed

--- src/Homework3.java
+++ src/Homework3.java
@@ -2,7 +2,10 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Homework3 {
+  public static Scanner scanner = new Scanner(System.in);
+  public static Random random = new Random();
 
   public static void main(String[] args) {
     questsNumber();
@@ -11,9 +14,7 @@ public class Homework3 {
 
   private static void questsStr() {
     String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-    Random random = new Random();
     var strChoose = words[random.nextInt(words.length)];
-    Scanner scanner = new Scanner(System.in);
     System.out.println("Угадайте слово:");
     while (true) {
       var strInput = scanner.next();
@@ -21,7 +22,7 @@ public class Homework3 {
         System.out.println("Вы выиграли");
         break;
       } else {
-        StringBuilder strPrint = new StringBuilder();
+        var strPrint = new StringBuilder();
         for (var i = 0; i < strInput.length(); i++) {
           if (strInput.charAt(i) == strChoose.charAt(i)) {
             strPrint.append(strInput.charAt(i));
@@ -35,18 +36,17 @@ public class Homework3 {
         System.out.println(strPrint);
       }
     }
+    scanner.close();
   }
 
   public static void questsNumber() {
-    gameQuestsNumber();
-
+    gameQuestsNumber(random.nextInt(9));
     while (true) {
-      Scanner scanner = new Scanner(System.in);
       System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
       try {
         var choose = scanner.nextInt();
         if (choose == 1) {
-          gameQuestsNumber();
+          gameQuestsNumber(random.nextInt(9));
         } else if (choose == 0) {
           break;
         } else {
@@ -58,11 +58,7 @@ public class Homework3 {
     }
   }
 
-
-  private static void gameQuestsNumber() {
-    Random random = new Random();
-    Scanner scanner = new Scanner(System.in);
-    var numberGuess = random.nextInt(9);
+  private static void gameQuestsNumber(int numberGuess) {
     var numberInput = 0;
     for (var i = 1; i <= 3; i++) {
       System.out.println("Угадайте число от 0 до 9:");
